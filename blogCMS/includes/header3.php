24c527cbf2569bd285e0c8451ea752e34de40f45
Hamish Tran <hamish.tran@gmail.com>
2018-05-23 12:42:34 +1000

    remove signup button from signup page

---
+++ blogCMS/includes/header3.php
@@ -0,0 +1,49 @@
+<?php
+
+	include ("config.php");
+	include ("db.php");
+	
+	$query = "SELECT * FROM categories";
+	
+	$categories = $db->query($query);
+	
+	
+	
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>BlogCMS</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <link href="css/blog.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-4 pt-1">
+            <a class="blog-header-logo text-dark" href="home.php">Tumble</a>
+          </div>
+          <div class="col-4 d-flex justify-content-end align-items-center">
+            <a class="text-muted" href="#"></a>
+            <a class="btn btn-sm btn-outline-secondary" href="login.php">Log In</a>
+   		  </div>
+        </div>
+      </header>
+	</div>
+  </body>
+</html>
